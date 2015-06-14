74bf5a56f057d6c81fb83a257efcd43d3262abf9
tropicloud <admin@tropicloud.net>
2015-06-13 21:37:27 -0300

    Sat Jun 13 21:37:27 BRT 2015

--- wpm/bin/wpm-wordpress.sh
+++ wpm/bin/wpm-wordpress.sh
@@ -2,7 +2,7 @@
 # WORDPRESS SETUP
 # ------------------------
 
-wpm_wordpress_setup() {
+wpm_wordpress() {
 
 	su -l $user -c "git clone https://github.com/roots/bedrock.git ."
 	su -l $user -c "composer install"

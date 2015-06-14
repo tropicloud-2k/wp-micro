c42a3bca9669c0c9ddd60653a89be6c097d46e30
tropicloud <admin@tropicloud.net>
2015-06-14 03:51:48 -0300

    Sun Jun 14 03:51:48 BRT 2015

--- wpm/bin/wpm-env.sh
+++ wpm/bin/wpm-env.sh
@@ -10,7 +10,7 @@ DB_USER=$user
 DB_PASSWORD=`cat /etc/.header_mustache`
 DB_HOST=127.0.0.1
 
-WP_ENV=development
+WP_ENV=production
 WP_HOME=$SCHEME://$HOSTNAME
 WP_SITEURL=$SCHEME://$HOSTNAME/wp
 END

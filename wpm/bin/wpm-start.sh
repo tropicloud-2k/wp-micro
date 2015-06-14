b4f2ad6846200cd11a3cf0f43b7334bce55381aa
tropicloud <admin@tropicloud.net>
2015-06-14 14:55:45 -0300

    Sun Jun 14 14:55:45 BRT 2015

--- wpm/bin/wpm-start.sh
+++ wpm/bin/wpm-start.sh
@@ -3,8 +3,12 @@
 # ------------------------
 
 wpm_start() {	
-	
-	wpm_header "Starting Services"
+
+	if [[  ! -d /var/lib/mysql  ]]; then wpm_mysql; fi
+	if [[  ! -d /var/www/web  ]]; then wpm_wordpress; fi
+	if [[  ! -f /var/www/web/.env   ]]; then wpm_env; fi
+
+	wpm_header "Service Startup"
 
 	if [[  -f /tmp/supervisord.pid  ]]; then
 	
@@ -12,14 +16,7 @@ wpm_start() {
 		then /usr/bin/supervisorctl start all;
 		else /usr/bin/supervisorctl start $2;
 		fi
-		
-	else
-	
-		if [[  ! -d /var/lib/mysql  ]];     then wpm_mysql; fi
-		if [[  ! -d /var/www/web  ]];       then wpm_wordpress; fi
-		if [[  ! -f /var/www/web/.env   ]]; then wpm_env; fi
-				
-		exec /usr/bin/supervisord -n -c /wpm/etc/supervisord.conf
-	
+
+	else exec /usr/bin/supervisord -n -c /wpm/etc/supervisord.conf	
 	fi
 }

a830930279bda8ad37c9f94b12d0ad6b32e04b29
tropicloud <admin@tropicloud.net>
2015-06-14 03:28:57 -0300

    Sun Jun 14 03:28:57 BRT 2015

--- wpm/bin/wpm-setup.sh
+++ wpm/bin/wpm-setup.sh
@@ -47,7 +47,7 @@ wpm_setup() {
 	# WP-CLI
 	# ------------------------
 	
-	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /usr/local/bin/wp
+	curl -sO https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /usr/local/bin/wp
 	chmod +x /usr/local/bin/wp
 		
 	# ------------------------

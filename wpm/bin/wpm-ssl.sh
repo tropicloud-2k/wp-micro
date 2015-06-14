9770143476dbe79e4ed2faf8f0828305a168b48e
tropicloud <admin@tropicloud.net>
2015-06-14 04:09:37 -0300

    Sun Jun 14 04:09:37 BRT 2015

--- wpm/bin/wpm-ssl.sh
+++ wpm/bin/wpm-ssl.sh
@@ -8,9 +8,9 @@ wpm_ssl() {
 	
 	cat /wpm/etc/nginx/openssl.conf | sed -e "s/example.com/$HOSTNAME/g" > openssl.conf
 	
-	openssl req -nodes -sha256 -newkey rsa:2048 -keyout app.key -out app.csr -config openssl.conf -batch
-	openssl rsa -in app.key -out app.key
-	openssl x509 -req -days 365 -sha256 -in app.csr -signkey app.key -out app.crt
+	openssl req -nodes -sha256 -newkey rsa:2048 -keyout $HOSTNAME.key -out $HOSTNAME.csr -config openssl.conf -batch
+	openssl rsa -in $HOSTNAME.key -out $HOSTNAME.key
+	openssl x509 -req -days 365 -sha256 -in $HOSTNAME.csr -signkey $HOSTNAME.key -out $HOSTNAME.crt
 	
 	rm -f openssl.conf
 			

df7bfddd2f819cc8ecb3970587d64486dad43314
tropicloud <admin@tropicloud.net>
2015-06-18 19:27:45 -0300

    Thu Jun 18 19:27:45 BRT 2015

--- wpm/bin/wpm-env.sh
+++ wpm/bin/wpm-env.sh
@@ -11,10 +11,9 @@ DB_PASSWORD=`cat /etc/.header_mustache`
 DB_HOST=127.0.0.1
 
 WP_ENV=$WP_ENV
-WP_HOME=$SCHEME://$HOSTNAME
-WP_SITEURL=$SCHEME://$HOSTNAME/wp
+WP_HOME=$WP_URL
+WP_SITEURL=$WP_URL/wp
 END
-
 	chown $user:nginx /var/wpm/.env
 	
 }

e9df7176c2555ee6266ab66658c5e1c9a648e79e
tropicloud <admin@tropicloud.net>
2015-06-20 05:05:54 -0300

    Sat Jun 20 05:05:54 BRT 2015

--- wpm/bin/wpm-wp-setup.sh
+++ wpm/bin/wpm-wp-setup.sh
@@ -50,8 +50,8 @@ wpm_wp_setup() {
 		
 		wpm_header "Backing Services"
 		
-		if [[  -n $MEMCACHE_PORT  ]]; then echo -e "\033[1;37m  Memcached:\033[0m $WP_MEMCACHE\033[0m"; fi
 		if [[  -n $REDIS_PORT  ]]; then echo -e "\033[1;37m  Redis:\033[0m $WP_REDIS\033[0m"; fi
+		if [[  -n $MEMCACHE_PORT  ]]; then echo -e "\033[1;37m  Memcached:\033[0m $WP_MEMCACHE\033[0m"; fi
 
 	fi
 

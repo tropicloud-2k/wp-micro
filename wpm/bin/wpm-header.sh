ac136bd30f30a29bd241e678cac87ed5969c6427
tropicloud <admin@tropicloud.net>
2015-06-20 12:07:12 -0300

    Sat Jun 20 12:07:12 BRT 2015

--- wpm/bin/wpm-header.sh
+++ wpm/bin/wpm-header.sh
@@ -22,4 +22,5 @@ wpm_listen() {
 		if [[  ! -z $REDIS_PORT  ]]; then echo -e "\033[1;32m  active:\033[0m Redis\033[1;37m @\033[0m $WPM_REDIS"; fi
 		if [[  ! -z $MEMCACHE_PORT  ]]; then echo -e "\033[1;32m  active:\033[0m Memcached\033[1;37m @\033[0m $WPM_MEMCACHE"; fi
 	fi
+	echo ""
 }

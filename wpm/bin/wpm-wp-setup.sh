4e5d5dc358ab98dbc9f5e4c312e97066d53a4ee8
tropicloud <admin@tropicloud.net>
2015-06-20 08:25:44 -0300

    Sat Jun 20 08:25:44 BRT 2015

--- wpm/bin/wpm-wp-setup.sh
+++ wpm/bin/wpm-wp-setup.sh
@@ -28,7 +28,7 @@ wpm_wp_setup() {
 	su -l $user -c "cd $wpm && composer install"
 	su -l $user -c "ln -s $web ~/"
 	
-	echo -e "WordPress installed successfully"
+	echo -e "Configuring environment"
 	
 	wpm_wp_install > /dev/null 2>&1
 	wpm_ssl $HOSTNAME

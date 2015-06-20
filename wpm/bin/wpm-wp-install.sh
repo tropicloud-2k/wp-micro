e7e3f752205c4be16b2ef8b5efad7dc0fba8b386
tropicloud <admin@tropicloud.net>
2015-06-20 02:03:11 -0300

    Sat Jun 20 02:03:10 BRT 2015

--- wpm/bin/wpm-wp-install.sh
+++ wpm/bin/wpm-wp-install.sh
@@ -4,20 +4,23 @@ wpm_wp_install() {
 	while [[  ! -e /run/mysqld/mysqld.sock  ]]; do sleep 1; done
 	
 	cd /var/wpm/web
+	
 	wp core install --allow-root --url=$WP_HOME --title=$WP_TITLE --admin_name=$WP_USER --admin_email=$WP_MAIL --admin_password=$WP_PASS
 	wp rewrite structure --allow-root '/%postname%/'
 	
 	if [[  -n $MEMCACHE_PORT  ]]; then 
-		su -l $user -c "cd /var/wpm/web && wp plugin install wp-ffpc -activate"
+		su -l $user -c "cd /var/wpm/web && wp plugin install wp-ffpc --activate"
 		sed -i "s/127.0.0.1:11211/$WP_MEMCACHE/g" /var/wpm/web/app/plugins/wp-ffpc/wp-ffpc.php
 		sed -i "s/'memcached'/'memcache'/g" /var/wpm/web/app/plugins/wp-ffpc/wp-ffpc.php
+		echo "define('WP_CACHE', true);" >> /var/wpm/config/environments/production.php
 	fi
 
-	if [[  -n $REDIS_PORT  ]]; then su -l $user -c "cd /var/wpm/web && wp plugin install redis-cache --activate"; fi
+	if [[  -n $REDIS_PORT  ]]; then 
+		su -l $user -c "cd /var/wpm/web && wp plugin install redis-cache --activate"
+		echo "define('WP_REDIS_HOST', getenv('WP_REDIS'));" >> /var/wpm/config/environments/production.php
+	fi
 	
 	sed -i '/DISALLOW_FILE_MODS/d' /var/wpm/config/environments/production.php
-	echo "define('WP_CACHE', true);" >> /var/wpm/config/environments/production.php
-	echo "define('WP_REDIS_HOST', getenv('WP_REDIS'));" >> /var/wpm/config/environments/production.php
 	
 	mysqladmin -u root shutdown
 }

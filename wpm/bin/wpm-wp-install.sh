98ffe88d2d4916c23aa813e3992ab72664d91156
tropicloud <admin@tropicloud.net>
2015-06-20 07:16:09 -0300

    Sat Jun 20 07:16:09 BRT 2015

--- wpm/bin/wpm-wp-install.sh
+++ wpm/bin/wpm-wp-install.sh
@@ -3,13 +3,13 @@
 # ------------------------
 
 wpm_wp_install() {
-mysqld_safe > /dev/null 2>&1 &
+
 # start mysql server
-	
-	while [[  ! -e /run/mysqld/mysqld.sock  ]]; do sleep 1; done
+mysqld_safe > /dev/null 2>&1 &
+while [[  ! -e /run/mysqld/mysqld.sock  ]]; do sleep 1; done
 	
 	cd $web && wpm_env
-
+	
 	if [[  ! -z "$WP_TITLE" && ! -z "$WP_USER" && ! -z "$WP_MAIL" && ! -z "$WP_PASS"  ]]; then 
 	wp core install --allow-root --url=$WP_HOME --title=$WP_TITLE --admin_name=$WP_USER --admin_email=$WP_MAIL --admin_password=$WP_PASS
 	wp rewrite structure --allow-root '/%postname%/'
@@ -19,4 +19,5 @@ mysqld_safe > /dev/null 2>&1 &
 
 # shutdown mysql server
 mysqladmin -u root shutdown
+
 }

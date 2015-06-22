683e97ecec6fc7308ad75b41454878633896c4cd
tropicloud <admin@tropicloud.net>
2015-06-22 07:03:26 -0300

    Mon Jun 22 07:03:26 BRT 2015

--- wpm/bin/wpm-build.sh
+++ wpm/bin/wpm-build.sh
@@ -83,5 +83,5 @@ wpm_build() {
 	chown -R $user:nginx /var/wpm
 	chmod +x /wpm/wpm.sh && ln -s /wpm/wpm.sh /usr/bin/wpm
 	
-	wpm_header "\033[0;32mDocker image successfully built"
+	wpm_header "Docker image successfully built"
 }

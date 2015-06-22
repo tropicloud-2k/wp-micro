0b12968f545ea52582725bb361f5f554c5767669
tropicloud <admin@tropicloud.net>
2015-06-22 09:42:46 -0300

    Mon Jun 22 09:42:46 BRT 2015

--- wpm/bin/wpm-common.sh
+++ wpm/bin/wpm-common.sh
@@ -16,12 +16,12 @@ wpm_header() {
 
 wpm_links() {
 	if [[  ! -z $REDIS_PORT  ]];
-	then echo -e "\033[1;32m  •\033[0;37m Redis\033[0m listening at `echo $REDIS_PORT | cut -d/ -f3`"		
-	else echo -e "\033[1;31m  •\033[0;37m Redis\033[0m not connected"
+	then echo -e "\n\033[1;32m  •\033[0;37m Redis\033[0m listening at `echo $REDIS_PORT | cut -d/ -f3`"		
+	else echo -e "\n\033[1;31m  •\033[0;37m Redis\033[0m not connected"
 	fi		
 	if [[  ! -z $MEMCACHED_PORT  ]];
-	then echo -e "\033[1;32m  •\033[0;37m Memcached\033[0m listening at `echo $MEMCACHED_PORT | cut -d/ -f3`"
-	else echo -e "\033[1;31m  •\033[0;37m Memcached\033[0m not connected"
+	then echo -e "\033[1;32m  •\033[0;37m Memcached\033[0m listening at `echo $MEMCACHED_PORT | cut -d/ -f3` \n"
+	else echo -e "\033[1;31m  •\033[0;37m Memcached\033[0m not connected \n"
 	fi
 }
 

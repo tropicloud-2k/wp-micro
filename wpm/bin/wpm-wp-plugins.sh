c55c6c07b8665787c5d6070d38d5223f648790e5
tropicloud <admin@tropicloud.net>
2015-06-22 06:51:55 -0300

    Mon Jun 22 06:51:55 BRT 2015

--- wpm/bin/wpm-wp-plugins.sh
+++ wpm/bin/wpm-wp-plugins.sh
@@ -9,12 +9,12 @@ wpm_wp_plugins() {
 		su -l $user -c "cd $web && wp plugin install wp-ffpc --activate"
 		sed -i "s/127.0.0.1:11211/$MEMCACHED/g" /etc/wpm/nginx.conf		
 		cat $web/app/plugins/wp-ffpc/wp-ffpc.php \
-		| sed -i "s/127.0.0.1:11211/$MEMCACHED/g" \
-		| sed -i "s/'300'/'3600'/g" \
-		| sed -i "s/'memcached'/'memcache'/g" \
-		| sed -i "s/'pingback_header'.*/'pingback_header' => true,/g" \
-		| sed -i "s/'response_header'.*/'response_header' => true,/g" \
-		| sed -i "s/'generate_time'.*/'generate_time' => true,/g" \
+		| sed "s/127.0.0.1:11211/$MEMCACHED/g" \
+		| sed "s/'300'/'3600'/g" \
+		| sed "s/'memcached'/'memcache'/g" \
+		| sed "s/'pingback_header'.*/'pingback_header' => true,/g" \
+		| sed "s/'response_header'.*/'response_header' => true,/g" \
+		| sed "s/'generate_time'.*/'generate_time' => true,/g" \
 		> $web/app/plugins/wp-ffpc/wp-ffpc.php
 		echo "define('WP_CACHE', true);" >> $wpm/config/environments/production.php
 	fi

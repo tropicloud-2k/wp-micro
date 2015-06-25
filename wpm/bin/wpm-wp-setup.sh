cc415d1de4603cc74101bf3a850cc80cb7cbced1
tropicloud <admin@tropicloud.net>
2015-06-25 10:59:33 -0300

    Thu Jun 25 10:59:33 BRT 2015

--- wpm/bin/wpm-wp-setup.sh
+++ wpm/bin/wpm-wp-setup.sh
@@ -40,13 +40,13 @@ wpm_wp_setup() {
 	su -l $user -c "cd $wpm && composer install"
 	su -l $user -c "ln -s $web ~/"
 	
-	wpm_wp_install		
-	
-# 	wpm_wp_install > /var/log/wpm-wp-install.log 2>&1 &		
-# 	wpm_wp_status() { cat /var/log/wpm-install.log | grep -q "WordPress setup completed"; }
-# 	
-# 	echo -ne "Initializing..."
-# 	while ! wpm_wp_status true; do echo -n '.' && sleep 1; done
-# 	echo -ne " done.\n"
+ 	wpm_wp_install > /var/log/wpm-wp-install.log 2>&1 &		
+ 	wpm_wp_status() { cat /var/log/wpm-install.log | grep -q "WordPress setup completed"; }
+ 	
+ 	echo -ne "Installing WordPress..."
+ 	while ! wpm_wp_status true; do echo -n '.' && sleep 1; done
+ 	echo -ne " done."
+ 	echo -e "Install log /var/log/wpm-wp-install.log"
+ 	echo -e "WordPress installed successfully"
 }
 

c09a0937b529517f868740e7c482e0b56240fec3
tropicloud <admin@tropicloud.net>
2015-06-25 06:43:02 -0300

    Thu Jun 25 06:43:02 BRT 2015

--- wpm/bin/wpm-common.sh
+++ wpm/bin/wpm-common.sh
@@ -16,16 +16,16 @@ wpm_header() {
 
 wpm_links() {
 	if [[  ! -z $MYSQL_PORT  ]];
-	then echo -e "\033[1;32m  •\033[0;37m MySQL\033[0m listening at `echo $MYSQL_PORT | cut -d/ -f3`"
-	else echo -e "\033[1;31m  •\033[0;37m MySQL\033[0m not connected"
+	then echo -e "\033[1;32m  •\033[0;33m MySQL\033[0m linked at `echo $MYSQL_PORT | cut -d/ -f3`"
+	else echo -e "\033[1;31m  •\033[0;33m MySQL\033[0m not linked"
 	fi	
 	if [[  ! -z $REDIS_PORT  ]];
-	then echo -e "\033[1;32m  •\033[0;37m Redis\033[0m listening at `echo $REDIS_PORT | cut -d/ -f3`"		
-	else echo -e "\033[1;31m  •\033[0;37m Redis\033[0m not connected"
+	then echo -e "\033[1;32m  •\033[0;33m Redis\033[0m linked at `echo $REDIS_PORT | cut -d/ -f3`"		
+	else echo -e "\033[1;31m  •\033[0;33m Redis\033[0m not linked"
 	fi		
 	if [[  ! -z $MEMCACHED_PORT  ]];
-	then echo -e "\033[1;32m  •\033[0;37m Memcached\033[0m listening at `echo $MEMCACHED_PORT | cut -d/ -f3`"
-	else echo -e "\033[1;31m  •\033[0;37m Memcached\033[0m not connected"
+	then echo -e "\033[1;32m  •\033[0;33m Memcached\033[0m linked at `echo $MEMCACHED_PORT | cut -d/ -f3`"
+	else echo -e "\033[1;31m  •\033[0;33m Memcached\033[0m not linked"
 	fi
 }
 
@@ -42,13 +42,13 @@ wpm_hostname() {
 
 wpm_hostname_true(){
 	wpm_header "Welcome to WP-Mini"
-	echo -e "\033[0m  Using \033[0;37m${HOSTNAME}\033[0m as hostname (domain). \n"
+	echo -e "\033[0m  Using \033[0;37m${HOSTNAME}\033[0m as hostname (domain)\n"
 	wpm_links
 }
 
 wpm_hostname_false(){
 	wpm_header "Error! Hostname is not set."
-	echo -e "\033[1;31m  Use the \033[1;37m-h\033[1;31m flag to set the hostname (domain).\n
+	echo -e "\033[1;31m  Use the \033[1;37m-h\033[1;31m flag to set the hostname (domain)\n
 \033[0m  Ex: docker run -P -h example.com -d tropicloud/wp-micro \n
 \033[0m  Aborting script...\n\n" && exit 1
 }

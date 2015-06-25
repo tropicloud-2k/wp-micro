37748edd67d24f5be79b98e3a87050db4207a4d5
tropicloud <admin@tropicloud.net>
2015-06-25 05:28:53 -0300

    Thu Jun 25 05:28:53 BRT 2015

--- wpm/bin/wpm-wp-install.sh
+++ wpm/bin/wpm-wp-install.sh
@@ -2,25 +2,24 @@
 # WORDPRESS INSTALL
 # ------------------------
 
+wpm_wp_core() {
+
+	wp core install --allow-root --url=$WP_HOME --title=$WP_TITLE --admin_name=$WP_USER --admin_email=$WP_MAIL --admin_password=$WP_PASS
+	wp rewrite structure --allow-root '/%postname%/'
+	wpm_wp_plugins
+}
+
 wpm_wp_install() {
 
-	wpm_environment
-	
-	cd $web
+	wpm_environment && cd $web
 	
 	if [[  ! -z "$WP_TITLE" && ! -z "$WP_USER" && ! -z "$WP_MAIL" && ! -z "$WP_PASS"  ]]; then 
-	
-		# start mysql server
-		mysqld_safe > /dev/null 2>&1 &
-		while [[  ! -e /run/mysqld/mysqld.sock  ]]; do sleep 1; done
-		
-		wp core install --allow-root --url=$WP_HOME --title=$WP_TITLE --admin_name=$WP_USER --admin_email=$WP_MAIL --admin_password=$WP_PASS
-		wp rewrite structure --allow-root '/%postname%/'
-		wpm_wp_plugins
-		
-		# stop mysql server
-		mysqladmin -u root shutdown
-		
+		if [[  -z $MYSQL_PORT  ]]; then
+			mysqld_safe > /dev/null 2>&1 &
+			while [[  ! -e /run/mysqld/mysqld.sock  ]]; do sleep 1; done && wpm_wp_core			
+			mysqladmin -u root shutdown
+		else wpm_wp_core
+		fi
 	fi
 	echo -e "$(date +%Y-%m-%d\ %T) WordPress setup completed" >> /var/log/wpm-install.log
 }

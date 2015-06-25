750cf72bec33c899abc6705d69277f6e78cc6657
tropicloud <admin@tropicloud.net>
2015-06-25 13:41:51 -0300

    Thu Jun 25 13:41:51 BRT 2015

--- wpm/bin/wpm-build.sh
+++ wpm/bin/wpm-build.sh
@@ -11,45 +11,8 @@ wpm_build() {
 		msmtp \
 		nginx \
 		openssl \
-		php-embed \
-		php-cgi \
-		php-mcrypt \
-		php-sqlite3 \
-		php-soap \
-		php-cli \
-		php-sockets \
-		php-xmlreader \
-		php-curl \
-		php-intl \
-		php-pgsql \
 		php-common \
-		php-pdo_pgsql \
-		php-pdo_sqlite \
-		php-phpdbg \
-		php-ldap \
-		php-zlib \
-		php-phar \
-		php-calendar \
-		php-dev \
-		php-zip \
-		php-xmlrpc \
-		php-dom \
-		php-mysqli \
-		php-pear \
-		php-mysql \
-		php-pdo_mysql \
-		php-json \
-		php-opcache \
 		php-memcache \
-		php-pdo \
-		php-gd \
-		php-openssl \
-		php-iconv \
-		php-gettext \
-		php-xml \
-		php-bz2 \
-		php-fpm \
-		php-ftp \
 		supervisor \
 		libmemcached \
 		nano curl git zip

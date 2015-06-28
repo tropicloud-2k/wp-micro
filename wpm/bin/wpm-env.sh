90b38a9241d2db582a3f40a7d54b28d0cc1308c0
tropicloud <admin@tropicloud.net>
2015-06-27 22:39:15 -0300

    Sat Jun 27 22:39:15 BRT 2015

--- wpm/bin/wpm-env.sh
+++ wpm/bin/wpm-env.sh
@@ -7,9 +7,9 @@ wpm_env() {
 	# hide "The mysql extension is deprecated and will be removed in the future: use mysqli or PDO"
 	sed -i "s/define('WP_DEBUG'.*/define('WP_DEBUG', false);/g" $WPS_WWW/config/environments/development.php
 
-	if [[  ! -z $WPS_MEMCACHED_PORT  ]]; then export MEMCACHED=`echo $WPS_MEMCACHED_PORT | cut -d/ -f3`; fi		
-	if [[  ! -z $WPS_REDIS_PORT  ]]; then export REDIS=`echo $WPS_REDIS_PORT | cut -d/ -f3`; fi
-	if [[  ! -z $MYSQL_PORT  ]]; then export MYSQL=`echo $MYSQL_PORT | cut -d/ -f3`; fi
+	if [[  ! -z $WPS_MEMCACHED_PORT  ]]; then export WPS_MEMCACHED=`echo $WPS_MEMCACHED_PORT | cut -d/ -f3`; fi		
+	if [[  ! -z $WPS_REDIS_PORT  ]]; then export WPS_REDIS=`echo $WPS_REDIS_PORT | cut -d/ -f3`; fi
+	if [[  ! -z $MYSQL_PORT  ]]; then export WPS_MYSQL=`echo $MYSQL_PORT | cut -d/ -f3`; fi
 	
 	if [[  $WP_SSL == 'true'  ]];
 	then export WP_HOME="https://${HOSTNAME}"
@@ -18,7 +18,7 @@ wpm_env() {
 	
 	export WP_SITEURL="${WP_HOME}/wp"
 	export WPS_PASS="`openssl rand 12 -hex`"
-	export WPS_SHA1="`echo -ne "$WPS_ENV_HTTP_PASS" | sha1sum | awk '{print $1}'`"
+	export WPS_SHA1="`echo -ne "$WPS_PASS" | sha1sum | awk '{print $1}'`"
 	export AUTH_KEY="`openssl rand 48 -base64`"
 	export SECURE_AUTH_KEY="`openssl rand 48 -base64`"
 	export LOGGED_IN_KEY="`openssl rand 48 -base64`"
@@ -42,7 +42,7 @@ wpm_env() {
 	
 	cat /wpm/etc/supervisord.conf \
 	| sed -e "s/example.com/$HOSTNAME/g" \
-	| sed -e "s/WPS_PASS/{SHA}$WPS_ENV_HTTP_SHA1/g" \
+	| sed -e "s/WPS_PASS/{SHA}$WPS_SHA1/g" \
 	> /etc/supervisord.conf && chmod 644 /etc/supervisord.conf
 
 	echo -e "$WPS_USER:`openssl passwd -crypt $WPS_ENV_HTTP_PASS`\n" > $WPS_HOME/.htpasswd
